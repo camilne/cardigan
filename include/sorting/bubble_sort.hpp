bc0da38f36e642abc350e2fb0826b808b8fd5cb6
camilne <cameronm213@gmail.com>
2018-04-26 21:40:35 -0600

    Add namespace to library

--- include/sorting/bubble_sort.hpp
+++ include/sorting/bubble_sort.hpp
@@ -3,48 +3,50 @@
 
 #include <algorithm>
 
-/*
-Bubble sort
--------------------
-Sorts a container by repeatedly swapping adjacent incorrect elements. Continues
-to sort while a swap is performed during a scan of the container.
+namespace cgn {
+    /*
+    Bubble sort
+    -------------------
+    Sorts a container by repeatedly swapping adjacent incorrect elements. Continues
+    to sort while a swap is performed during a scan of the container.
 
-RandomIt must be an iterator that allows random access.
+    RandomIt must be an iterator that allows random access.
 
-Compare must implement a function, compare(a, b), such that the container is
-sorted when, for every element, compare(a_i, a_{i-1}), is false.
+    Compare must implement a function, compare(a, b), such that the container is
+    sorted when, for every element, compare(a_i, a_{i-1}), is false.
 
-Time complexity
----------------
-Best case: O(n), where n is the number of elements
-Average case: O(n^2)
-Worst case: O(n^2)
+    Time complexity
+    ---------------
+    Best case: O(n), where n is the number of elements
+    Average case: O(n^2)
+    Worst case: O(n^2)
 
-Space complexity
-----------------
-O(1)
-*/
-template <class RandomIt, class Compare>
-void bubble_sort(RandomIt first, RandomIt last, Compare compare) {
-    if(first == last)
-        return;
+    Space complexity
+    ----------------
+    O(1)
+    */
+    template <class RandomIt, class Compare>
+    void bubble_sort(RandomIt first, RandomIt last, Compare compare) {
+        if(first == last)
+            return;
 
-    for(auto i = first + 1; i != last; ++i) {
-        auto swapped = false;
-        for(auto j = first + 1; j != last; ++j) {
-            if(!compare(*(j - 1), *j)) {
-                swapped = true;
-                std::swap(*(j - 1), *j);
+        for(auto i = first + 1; i != last; ++i) {
+            auto swapped = false;
+            for(auto j = first + 1; j != last; ++j) {
+                if(!compare(*(j - 1), *j)) {
+                    swapped = true;
+                    std::swap(*(j - 1), *j);
+                }
             }
+            if(!swapped)
+                break;
         }
-        if(!swapped)
-            break;
     }
-}
 
-template <class RandomIt>
-void bubble_sort(RandomIt first, RandomIt last) {
-    bubble_sort(first, last, [](auto a, auto b){ return a < b; });
+    template <class RandomIt>
+    void bubble_sort(RandomIt first, RandomIt last) {
+        bubble_sort(first, last, [](auto a, auto b){ return a < b; });
+    }
 }
 
 #endif
